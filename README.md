6f8f0186d1e0cbd336fefadd7235a6e1f6af8219
sami jaghouar <sami.jaghouar@hotmail.fr>
2021-12-31 15:40:32 +0100

    first version

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # i3wallpaper
-
 easily manage you wallpaper dynamically with i3wallpaper
+
+You cane have one wallpaper per workspace
+
+## How to install ?
+
+```
+pip install git+https://github.com/samsja/i3wallpaper
+```
+
+then create a config files in ~/.config/i3wallapaper/config.toml
+
+ex:
+
+```toml
+[wallpapers]
+1 = "path_to_your_wallpaper1"
+1 = "path_to_your_wallpaper2"
+```
+
--- i3wallpaper/main.py
+++ i3wallpaper/main.py
@@ -1,3 +1,40 @@
+from i3ipc import Connection, Event, WorkspaceEvent
+import os
+import toml
+from typing import Dict
+
+
+def load_config() -> Dict:
+    try:
+        config = toml.load(f"{os.path.expanduser('~')}/.config/i3wallpaper/config.toml")
+    except FileNotFoundError as e:
+        print("Eror: Can't locate config file")
+        print(e)
+        print("please create a config file in  ~/.config/i3wallpaper/config.toml")
+        exit()
+    return config
+
+
+def change_wp(current_workspace: str, wallpapers: Dict[str,str]):
+    img = wallpapers.get(current_workspace, wallpapers["1"])
+    os.system(f"feh --bg-fill {img}")
+
 
 def main():
-    print("hw")
+
+    config = load_config()
+    i3 = Connection()
+
+    wallpapers = config["wallpapers"]
+
+    def on_workspace_focus(self, event: WorkspaceEvent):
+        change_wp(event.current.name, wallpapers)
+
+    i3.on(Event.WORKSPACE_FOCUS, on_workspace_focus)
+    # Start the main loop and wait for events to come in.
+    i3.main()
+
+
+if __name__ == "__main__":
+
+    main()
